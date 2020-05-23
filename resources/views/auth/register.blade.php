b5cab12a8fa794333a26bbb74dfde08f64f41540
jorsh <cuentaparacosas123@hotmail.com>
2020-05-22 23:06:57 -0500

    Login medio hecho

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,78 @@
-@extends('layouts.app')
+@extends('layouts.nav')
+
+@section('style')
+<link rel="stylesheet" href="{{ asset('css/estilos-form.css') }}">
+<link rel="stylesheet" href="{{ asset('css/estilos-boton-s.css') }}">
+
+@endsection
 
 @section('content')
+
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+            <div class="panel panel-dafault">
             </div>
         </div>
+
     </div>
 </div>
+<section>
+    <div class="containerF">
+        <form method="POST" action="{{ route('register') }}">
+            {{ csrf_field() }}
+            <h1>Registro</h1>
+            <label class="{{ $errors->has('name') ? ' s-error' : '' }}" for="name">Nombre:</label>
+            <input class="{{ $errors->has('name') ? ' s-error' : '' }}" type="text" id="name" name="name" required
+                autofocus>
+            @if ($errors->has('name'))
+            <div class="m-error">
+                <div>
+                    <span><strong>{{ $errors->first('name') }}</strong></span>
+                </div>
+            </div>
+            @endif
+
+            <label class="{{ $errors->has('email') ? ' s-error' : '' }}" for="email">Correo:</label>
+            <input class="{{ $errors->has('email') ? ' s-error' : '' }}" type="email" id="email" name="email" required>
+            @if ($errors->has('email'))
+            <div class="m-error">
+                <div>
+                    <span><strong>{{ $errors->first('email') }}</strong></span>
+                </div>
+            </div>
+            @endif
+
+            <label class="{{ $errors->has('password') ? ' s-error' : '' }}" for="password">Contraseña:</label>
+            <input class="{{ $errors->has('password') ? ' s-error' : '' }}" type="password" id="password"
+                name="password" required>
+            @if ($errors->has('password'))
+            <div class="m-error">
+                <div>
+                    <span><strong>{{ $errors->first('password') }}</strong></span>
+                </div>
+            </div>
+            @endif
+
+            <label for="password_confirm">Confirmar Contraseña:</label>
+            <input type="password" id="password_confirm" name="password_confirmation" required>
+            @if ($errors->has('password_confirm'))
+            <div class="m-error">
+                <div>
+                    <span><strong>{{ $errors->first('password_confirm') }}</strong></span>
+                </div>
+            </div>
+            @endif
+
+            <button type="submit" class="btn-s">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Registrarse
+            </button>
+
+        </form>
+    </div>
+</section>
 @endsection
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -3,7 +3,11 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>Sispres</title>
     <link rel="stylesheet" href="{{ asset('css/estilos-nav.css') }}">
     <link rel="stylesheet" href="{{ asset('css/fonts.css') }}">
